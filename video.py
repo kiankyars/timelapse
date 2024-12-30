2685eaa60d60ad4b0775b2121527f91e048a5dcb
Kian Kyars <kiankyars@gmail.com>
2024-12-29 19:37:18 -0700

    sorted output list (what a blunder), and also add elapsed time + total time as parameters to the final timelapse output, so happy now, I don't need to buy a new webcam, it was just a problem with my video formatting

--- video.py
+++ video.py
@@ -13,13 +13,24 @@ date = sys.argv[1]
 TIMELAPSE_DIR = os.getenv('TIMELAPSE_DIR', str(Path.home() / 'Desktop' / 'timelapses'))
 OUTPUT_DIR = os.getenv('OUTPUT_DIR', str(Path.home() / 'Desktop'))
 FPS = int(os.getenv('FPS', 60))
+INTERVAL = int(os.getenv('INTERVAL', 5))
 
 # Define the folder containing images and the output video file
 image_folder = f"{TIMELAPSE_DIR}/timelapse_{date}"
-output_file = f"{OUTPUT_DIR}/timelapse.mov"
+
 
 # Get sorted list of image files
-images = [img for img in os.listdir(image_folder) if img.endswith(".jpg")]
+images = [img for img in os.listdir(image_folder)]
+images.sort()
+# Extract times from first and last images
+first_time = images[0].split('_')[1].split('.')[0]
+last_time = images[-1].split('_')[1].split('.')[0]
+elapsed_time = int(last_time) - int(first_time)
+# total time in seconds
+total_time = INTERVAL * len(images)
+# total time in minutes
+total_time //= 60
+
 
 # Ensure there are images to process
 if not images:
@@ -31,6 +42,7 @@ height, width, _ = first_image.shape
 
 # Define video writer
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+output_file = f"{OUTPUT_DIR}/timelapse_{date}_{elapsed_time}_{total_time}.mp4"
 video = cv2.VideoWriter(output_file, fourcc, FPS, (width, height))
 
 # Write images to the video
